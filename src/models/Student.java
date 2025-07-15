5747654b2f618a9b63a5237f12b78b1550aee0bf
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-15 18:45:55 +0400

    Creation of Student model

---
+++ src/models/Student.java
@@ -0,0 +1,44 @@
+package models;
+
+import abstracts.Person;
+
+import java.io.Serializable;
+
+public class Student extends Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private double gpa;
+    private Teacher teacher;
+
+    public Student(String pin, String firstName, String lastName, double gpa, Teacher teacher) {
+        super(pin, firstName, lastName);
+        this.gpa = gpa;
+        this.teacher = teacher;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public Student setGpa(double gpa) {
+        this.gpa = gpa;
+        return this;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public Student setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "PIN: " + getPin() + "\n" +
+                "First name: " + getFirstName() + "\n" +
+                "Last name: " + getLastName() + "\n" +
+                "GPA: " + gpa + "\n" +
+                "Teacher: " + teacher.getFirstName() + " " + teacher.getLastName();
+    }
+}
