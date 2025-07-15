3c07609f49d59d9ce4e29429cdd9511f38bd8750
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-15 18:35:53 +0400

    Implement inheritance to Teacher model

--- src/models/Teacher.java
+++ src/models/Teacher.java
@@ -1,47 +1,17 @@
 package models;
 
+import abstracts.Person;
+
 import java.io.Serializable;
 import java.util.List;
 
-public class Teacher implements Serializable {
+public class Teacher extends Person implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String pin;
-    private String firstName;
-    private String lastName;
     private List<Subject> subjects;
 
     public Teacher(String pin, String firstName, String lastName, List<Subject> subjects) {
-        this.pin = pin;
-        this.firstName = firstName;
-        this.lastName = lastName;
+        super(pin, firstName, lastName);
         this.subjects = subjects;
-    }
-
-    public String getPin() {
-        return pin;
-    }
-
-    public Teacher setPin(String pin) {
-        this.pin = pin;
-        return this;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Teacher setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Teacher setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
     }
 
     public List<Subject> getSubjects() {
@@ -55,6 +25,6 @@ public class Teacher implements Serializable {
 
     @Override
     public String toString() {
-        return "PIN: " + pin + "\nFirst name: " + firstName + "\nLast name: " + lastName;
+        return "PIN: " + getPin() + "\nFirst name: " + getFirstName() + "\nLast name: " + getLastName();
     }
 }
