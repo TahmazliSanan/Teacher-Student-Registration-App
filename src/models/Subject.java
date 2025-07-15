4c4e0095084b6ce7e964a6ad50ccdc06ca4bd618
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-15 18:24:00 +0400

    Creation of Subject model

---
+++ src/models/Subject.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String pin;
+    private String name;
+
+    public Subject(String pin, String name) {
+        this.pin = pin;
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public Subject setPin(String pin) {
+        this.pin = pin;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Subject setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "PIN: " + pin + "\nName: " + name;
+    }
+}
