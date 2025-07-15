95930e603e2d9a078c568269aeb606e21700bc86
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-15 22:38:14 +0400

    Creation of BaseService

---
+++ src/services/BaseService.java
@@ -0,0 +1,11 @@
+package services;
+
+import java.util.List;
+
+public interface BaseService {
+    void create();
+    void getByPin();
+    void getList();
+    void update();
+    void delete();
+}
