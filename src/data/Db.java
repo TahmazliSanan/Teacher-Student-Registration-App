3e079e8ec7ca77bc59c9d40656dfb36d6cf6456f
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-15 20:17:35 +0400

    Creation of Db class

---
+++ src/data/Db.java
@@ -0,0 +1,14 @@
+package data;
+
+import models.Student;
+import models.Subject;
+import models.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Db {
+    public static List<Teacher> teachers = new ArrayList<>();
+    public static List<Student> students = new ArrayList<>();
+    public static List<Subject> subjects = new ArrayList<>();
+}
