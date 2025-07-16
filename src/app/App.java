6498691213d2738ccd1af62546e43036e6b25a01
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-16 11:41:35 +0400

    Re-design App class

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,20 @@
 package app;
 
-public class App {
-    public static void main(String[] args) {
+import services.BaseService;
+import services.StudentService;
+import services.SubjectService;
+import services.TeacherService;
+import utils.MenuUtil;
 
+import java.util.Scanner;
+
+public class App {
+    private static final BaseService subjectService = new SubjectService();
+    private static final BaseService teacherService = new TeacherService();
+    private static final BaseService studentService = new StudentService();
+
+    public static void main(String[] args) {
+        MenuUtil menuUtil = new MenuUtil(new Scanner(System.in));
+        menuUtil.showServiceMenu(subjectService, teacherService, studentService);
     }
 }
